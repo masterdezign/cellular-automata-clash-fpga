f0655b03a4e9faddafdfb697cd35247213b42ebe
Bogdan <masterdezign@gmail.com>
2017-12-06 14:37:17 +0100

    Upgrade the testBench to Clash v0.99 (fails)

--- src/Test/Cellular.hs
+++ src/Test/Cellular.hs
@@ -1,34 +1,42 @@
 module Test.Cellular where
 
-import CLaSH.Prelude
+import Clash.Prelude
 import FPGA.CellularArray2
 
-topEntity :: Signal () -> Signal (BitVector 10)
+topEntity
+  :: SystemClockReset
+  => Signal System () -> Signal System (BitVector 10)
 topEntity _ = r
-  where ca = cArray3 d10 (Rule 110) 1 r
+  where ca = cArray3 d10 (Rule (0b110 :: BitVector 3)) 1 r
 
-        cnt = register 0 (cnt + 1) :: Signal (Unsigned 2)
+        cnt = register 0 (cnt + 1) :: Signal System (Unsigned 2)
         r = regEn 0 (cnt .==. 0) ca
 
-expectedOutput = outputVerifier $(v [
-                                   0b0000000000 :: BitVector 10
-                                 , 0b0000000001
-                                 , 0b0000000001
-                                 , 0b0000000001
-                                 , 0b0000000001
-                                 , 0b0000000011
-                                 , 0b0000000011
-                                 , 0b0000000011
-                                 , 0b0000000011
-                                 , 0b0000000111
-                                 , 0b0000000111
-                                 , 0b0000000111
-                                 , 0b0000000111
-                                 , 0b0000001101
-                                 , 0b0000001101
-                                 , 0b0000001101
-                                 , 0b0000001101
-                                 ] )
+testBench :: Signal System Bool
+testBench = done'
+  where
+    testInput    = stimuliGenerator (replicate d11 ())
+    expectOutput = outputVerifier expected
+    done         = expectOutput (topEntity testInput)
+    done'        = withClockReset (tbSystemClockGen (not <$> done')) systemResetGen done
 
-
-runTest = sampleN 17 $ expectedOutput $ topEntity $ stimuliGenerator $ replicate d11 ()
+expected :: Vec 17 (BitVector 10)
+expected = (  0b0000000000
+           :> 0b0000000001
+           :> 0b0000000001
+           :> 0b0000000001
+           :> 0b0000000001
+           :> 0b0000000011
+           :> 0b0000000011
+           :> 0b0000000011
+           :> 0b0000000011
+           :> 0b0000000111
+           :> 0b0000000111
+           :> 0b0000000111
+           :> 0b0000000111
+           :> 0b0000001101
+           :> 0b0000001101
+           :> 0b0000001101
+           :> 0b0000001101
+           :> Nil
+           )
